0d7ee2ff001d71f7e72aa34e5813f432abfc8a40
Laura-th1 <laut15@outlook.com>
2026-03-19 14:35:49 -0400

    prueba dos voice

--- controllers/voice.php
+++ controllers/voice.php
@@ -1,7 +1,10 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 header("Content-Type: text/xml");
 
 echo "<Response>";
-echo "<Say>Hola, esto es una prueba. Tu sistema ya funciona.</Say>";
+echo "<Say>Prueba con errores activados</Say>";
 echo "</Response>";
 ?>
