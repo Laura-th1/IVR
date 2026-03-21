ef9cb6e3c51e45933f2da936d625f53283465a9a
Laura-th1 <laut15@outlook.com>
2026-03-21 01:27:22 -0400

    guardar respuestas

--- controllers/voice.php
+++ controllers/voice.php
@@ -2,8 +2,9 @@
 header("Content-Type: text/xml");
 
 echo "<Response>";
-echo "<Say voice='Polly.Lupe'>Hola, dime tu nombre después del tono.</Say>";
-echo "<Gather input='speech' method='POST' action='https://ivr-3knv.onrender.com/controllers/process.php?step=1'>";
-echo "</Gather>";
+echo "<Say>Hola, dime tu nombre después del tono.</Say>";
+
+echo "<Gather input='speech' method='POST'
+      action='https://ivr-3knv.onrender.com/controllers/process.php?step=1'></Gather>";
+
 echo "</Response>";
-?>
