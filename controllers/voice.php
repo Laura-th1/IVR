cf519b7e83491e393d7e30a4f50592e8d49f780e
Laura-th1 <laut15@outlook.com>
2026-03-21 01:04:07 -0400

    testing diferent voice

--- controllers/voice.php
+++ controllers/voice.php
@@ -2,7 +2,7 @@
 header("Content-Type: text/xml");
 
 echo "<Response>";
-echo "<Say>Hola, dime tu nombre después del tono.</Say>";
+echo "<Say voice='Polly.Lupe'>Hola, dime tu nombre después del tono.</Say>";
 echo "<Gather input='speech' method='POST' action='https://ivr-3knv.onrender.com/controllers/process.php?step=1'>";
 echo "</Gather>";
 echo "</Response>";
