ed4cde2bff7425089f4804a3764bee0fbd59e9b7
Laura-th1 <laut15@outlook.com>
2026-03-21 15:10:39 -0400

    deleted ver_datos.php

--- ver_datos.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "config/bd.php";
-
-$result = pg_query($conn, "SELECT * FROM respuestas");
-
-while ($row = pg_fetch_assoc($result)) {
-    echo $row['telefono'] . " - " . $row['pregunta'] . " - " . $row['respuesta'] . "<br>";
-}
-?>
