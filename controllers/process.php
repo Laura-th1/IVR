ac7cbc580b42c8d15bf8e6fe238de86cebbde06f
Laura-th1 <laut15@outlook.com>
2026-03-26 12:16:23 -0400

    Changes in process.php

--- controllers/process.php
+++ controllers/process.php
@@ -4,7 +4,7 @@ require_once "../config/bd.php";
 header("Content-Type: text/xml");
 
 //debug temporal
-file_put_contents("log.txt", print_r($_POST, true));
+@file_put_contents("log.txt", print_r($_POST, true) . "\n--- " . date('Y-m-d H:i:s') . " ---\n", FILE_APPEND);
 
 $telefono = $_POST['From'] ?? '';
 $respuesta = $_POST['SpeechResult'] ?? '';
@@ -23,21 +23,29 @@ echo "<Response>";
 if ($step == 1) {
 
     // Guardar nombre
-    pg_query($conn, "INSERT INTO respuestas (telefono, pregunta, respuesta)
-                     VALUES ('$telefono', 'Nombre', '$respuesta')");
+    $query = "INSERT INTO respuestas (telefono, pregunta, respuesta) VALUES ('" . pg_escape_string($telefono) . "', 'Nombre', '" . pg_escape_string($respuesta) . "')";
+    $result = pg_query($conn, $query);
+    
+    if (!$result) {
+        echo "<Say voice='Polly.Lupe'>Error al guardar. Intenta nuevamente.</Say>";
+    } else {
+        echo "<Say voice='Polly.Lupe'>Gracias $respuesta. Ahora dime tu edad.</Say>";
+    }
 
-    echo "<Say voice='Polly.Lupe'>Gracias $respuesta. Ahora dime tu edad.</Say>";
-
-    echo "<Gather input='speech' method='POST'
+    echo "<Gather input='speech' method='POST' timeout='5'
           action='https://ivr-3knv.onrender.com/controllers/process.php?step=2'></Gather>";
 
 } elseif ($step == 2) {
 
     // Guardar edad
-    pg_query($conn, "INSERT INTO respuestas (telefono, pregunta, respuesta)
-                     VALUES ('$telefono', 'Edad', '$respuesta')");
-
-    echo "<Say voice='Polly.Lupe'> Gracias. Tus datos han sido guardados correctamente.</Say>";
+    $query = "INSERT INTO respuestas (telefono, pregunta, respuesta) VALUES ('" . pg_escape_string($telefono) . "', 'Edad', '" . pg_escape_string($respuesta) . "')";
+    $result = pg_query($conn, $query);
+    
+    if (!$result) {
+        echo "<Say voice='Polly.Lupe'>Error al guardar edad.</Say>";
+    } else {
+        echo "<Say voice='Polly.Lupe'>Gracias. Tus datos han sido guardados correctamente.</Say>";
+    }
 }
 
 echo "</Response>";
