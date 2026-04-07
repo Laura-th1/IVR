eae8b4af58bfef26b488df6b7a9aa8354d79f471
Laura-th1 <laut15@outlook.com>
2026-04-06 22:23:58 -0500

    corrección del codigo de chat_id por el grupo

--- controllers/process.php
+++ controllers/process.php
@@ -87,7 +87,7 @@ if ($step == 1) {
            TELEGRAM (REEMPLAZO DE TWILIO)
            ========================= */
         $token = getenv("TELEGRAM_TOKEN");
-$chat_id = "7050620400";
+$chat_id = "-4994123276"; // Reemplaza con tu chat ID
 
 $mensaje = "📞 Nuevo registro IVR:\n";
 $mensaje .= "Telefono: $telefono\n";
