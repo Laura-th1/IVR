a6d9f03e6bc4bb65508ec383eebb85a9afd45a07
Enriquemio <milagrosdanger11@gmail.com>
2026-04-18 02:33:03 -0400

    prueba voice ai php

--- controllers/voice_ai.php
+++ controllers/voice_ai.php
@@ -1,20 +1,17 @@
 <?php
 header("Content-Type: text/xml");
 
-echo "<Response>";
-
-echo "<Say voice='Polly.Lupe'>
-Hola, bienvenido a By Wifer. Puedes decir tu reserva completa después del tono.
-</Say>";
-
-
-
-echo '<Gather 
-        input="speech"
+echo '<Response>
+<Gather input="speech"
         language="es-ES"
         speechTimeout="auto"
         method="POST"
-        action="https://ivr-3knv.onrender.com/controllers/process_ai.php/">
-      </Gather>';
-
-echo "</Response>";
+        action="https://ivr-3knv.onrender.com/controllers/process_ai.php">
+    <Say voice="Polly.Lupe">
+        Hola, gracias por llamar a By Wifer. Dime tu reserva completa, por ejemplo:
+        Enrique, mañana a las ocho, para cuatro personas.
+    </Say>
+</Gather>
+<Say voice="Polly.Lupe">No recibí respuesta. Por favor vuelve a llamar.</Say>
+</Response>';
+?>
